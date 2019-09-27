7090f8063bc095460fc7aa47b92e762259231432
Sean Belverstone <seanbelverstone@gmail.com>
2019-09-27 16:35:39 -0500

    Added some text pulled from my old portfolio and added it below the jumbotron. Also added some styling just to give myself an idea of what its going to look like at the end. I feel this assists me in the creative process. Need to add image of my face and github/linkedin/cv links

--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -4,12 +4,25 @@ import { Jumbotron, Container } from 'reactstrap';
 const MainSection = (props) => {
   return (
     <div>
-      <Jumbotron fluid>
-        <Container fluid>
-          <h1 className="display-3">From Full English to Full Stack</h1>
-          <p className="lead">My personal journey from chef to coder.</p>
-        </Container>
-      </Jumbotron>
+        <div className="row">
+            <div className="col-12">
+                <Jumbotron fluid>
+                    <Container fluid>
+                    <h1 className="display-3">From Full English to Full Stack</h1>
+                    <p className="lead">My personal journey from chef to coder.</p>
+                    </Container>
+                </Jumbotron>
+            </div>
+        </div>
+        <div className="row">
+            <div className="col-12">
+                <div id="aboutMeSection">
+                    <p>As an Englishman who's spent most of his career in the catering industry, I've made a smooth transition over to web development. After completing a Full Stack Web Developer bootcamp at UT Austin, I am now proficient in React, Javascript, Node.js, Express, HTML & CSS, among many other programs, libraries and frameworks.</p>
+                    <p>With a keen eye for designing front-end and a logical brain for tackling back-end, I'd be an essential asset to your team.</p>
+                    <p>Feel free to peruse my favorite projects via the navbar above, or even visit my GitHub to see my full catalogue. I'd love to hear from you, too! Fire me an email from the contact page.</p>
+                </div>
+            </div>
+        </div>
     </div>
   );
 };
