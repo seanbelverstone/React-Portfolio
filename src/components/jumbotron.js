bc5fe61e3f2ec56bd8e2d608e5ff749768a66ec1
Sean Belverstone <seanbelverstone@gmail.com>
2019-09-27 17:19:38 -0500

    Edited the jumbotron some more. Added an about me section, an image of myself and also some images into the images folder. I also added links to my github, linkedin profile and cv

--- src/components/jumbotron.js
+++ src/components/jumbotron.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Jumbotron, Container } from 'reactstrap';
+import image from "../images/about-me-photo.jpg";
+import githubLogo from "../images/GitHub-Mark-120px-plus.png";
+import linkedInLogo from "../images/linkedin.png";
 
 const MainSection = (props) => {
   return (
@@ -10,12 +13,25 @@ const MainSection = (props) => {
                     <Container fluid>
                     <h1 className="display-3">From Full English to Full Stack</h1>
                     <p className="lead">My personal journey from chef to coder.</p>
+
+                    <a className="logo" href="https://github.com/seanbelverstone">
+                        <img src={githubLogo} alt="GitHub logo" id="github" />
+                    </a>
+                    <a className="logo" href="https://www.linkedin.com/in/sean-belverstone/">
+                        <img src={linkedInLogo} alt="LinkedIn logo" id="linkedin" />
+                    </a>
+                    <a className="logo" href="https://drive.google.com/file/d/1fXTNVPvD0wRBIUyHc-Y8QWaxbEOjTLP0/view?usp=sharing" id="cv">
+                        CV
+                    </a>
                     </Container>
                 </Jumbotron>
             </div>
         </div>
         <div className="row">
-            <div className="col-12">
+            <div className="col-3">
+                <img src={image} alt="Sean Belverstone" />
+            </div>
+            <div className="col-9">
                 <div id="aboutMeSection">
                     <p>As an Englishman who's spent most of his career in the catering industry, I've made a smooth transition over to web development. After completing a Full Stack Web Developer bootcamp at UT Austin, I am now proficient in React, Javascript, Node.js, Express, HTML & CSS, among many other programs, libraries and frameworks.</p>
                     <p>With a keen eye for designing front-end and a logical brain for tackling back-end, I'd be an essential asset to your team.</p>
