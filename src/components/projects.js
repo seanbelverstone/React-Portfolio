c412539436c2ae190807d47c41cb9df4fc3b4c2b
Sean Belverstone <seanbelverstone@gmail.com>
2019-09-27 20:20:35 -0500

    Started importing all of the images that ill need for the carousel. Realised Im missing a bunch so currently booting up all my previous projects to take screenshots. Will probably add text later

--- src/components/projects.js
+++ src/components/projects.js
@@ -6,22 +6,79 @@ import {
   CarouselIndicators,
   CarouselCaption
 } from 'reactstrap';
+import fureverHome from "../images/fureverHome";
+import burger from "../images/burger";
+import clicky from "../images/clicky";
+import friendFinder from "../images/friendFinder";
+import bamazon from "../images/bamazon.jpg";
+import crystal from "../images/crystalcollector.png";
+import fridgeChef from "../images/fridgechef.png";
+import giftastic from "../images/giftastic.png";
+import hangman from "../images/hangman.png";
+import liri from "../images/liri-node-app.png";
+import trainTimes from "../images/traintimes.png";
+import trivia from "../images/trivia.png";
 
 const items = [
   {
-    src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa1d%20text%20%7B%20fill%3A%23555%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa1d%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23777%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22285.921875%22%20y%3D%22218.3%22%3EFirst%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-    altText: 'Slide 1',
-    caption: 'Slide 1'
+    src: fureverHome,
+    altText: 'FurEver Home',
+    caption: 'Enter Description Here'
   },
   {
-    src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa20%20text%20%7B%20fill%3A%23444%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa20%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23666%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22247.3203125%22%20y%3D%22218.3%22%3ESecond%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-    altText: 'Slide 2',
-    caption: 'Slide 2'
+    src: burger,
+    altText: 'Sequelized Burger',
+    caption: 'Enter Description Here'
   },
   {
-    src: 'data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_15ba800aa21%20text%20%7B%20fill%3A%23333%3Bfont-weight%3Anormal%3Bfont-family%3AHelvetica%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_15ba800aa21%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23555%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22277%22%20y%3D%22218.3%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E',
-    altText: 'Slide 3',
-    caption: 'Slide 3'
+    src: clicky,
+    altText: 'Halo Themed Clicky Game',
+    caption: 'Enter Description Here'
+  },
+  {
+    src: friendFinder,
+    altText: 'Friend Finder',
+    caption: 'Enter Description Here'
+  },
+  {
+    src: bamazon,
+    altText: 'Bamazon',
+    caption: 'Enter Description Here'
+  },
+  {
+    src: liri,
+    altText: 'LIRI - Node App',
+    caption: 'Enter Description Here'
+  },
+  {
+    src: fridgeChef,
+    altText: 'Fridge Chef',
+    caption: 'Enter Description Here'
+  },
+  {
+    src: trivia,
+    altText: 'Lord of the Rings Trivia',
+    caption: 'Enter Description Here'
+  },
+  {
+    src: trainTimes,
+    altText: 'Train Times',
+    caption: 'Enter Description Here'
+  },
+  {
+    src: giftastic,
+    altText: 'GifTastic',
+    caption: 'Enter Description Here'
+  },
+  {
+    src: crystal,
+    altText: 'Crystal Collector',
+    caption: 'Enter Description Here'
+  },
+  {
+    src: hangman,
+    altText: 'UK Hangman',
+    caption: 'Enter Description Here'
   }
 ];
 
