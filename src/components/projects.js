c38b57c0a943c1937b216a500df53564b31508a7
Sean Belverstone <seanbelverstone@gmail.com>
2019-09-28 16:14:26 -0500

    Added a "stop animating on hover" function to the carousel. I thought this was an intuitive move because it allows the user to read the text if theyre hovering on it. Pretty much finished the projects page, just need to add more text. Will add later

--- src/components/projects.js
+++ src/components/projects.js
@@ -27,73 +27,85 @@ const items = [
     id: 1,
     src: fureverHome,
     altText: 'FurEver Home',
-    caption: 'Enter Description Here'
+    caption: 'Enter Description Here',
+    link: "https://github.com/seanbelverstone/FurEver-Home"
   },
   {
     id: 2,
     src: burger,
     altText: 'Sequelized Burger',
-    caption: 'Enter Description Here'
+    caption: 'Enter Description Here',
+    link: "https://github.com/seanbelverstone/sequelizedBurger"
   },
   {
     id: 3,
     src: clicky,
     altText: 'Halo Themed Clicky Game',
-    caption: 'Enter Description Here'
+    caption: 'Enter Description Here',
+    link: "https://github.com/seanbelverstone/Clicky-Game"
   },
   {
     id: 4,
     src: friendFinder,
     altText: 'Friend Finder',
-    caption: 'Enter Description Here'
+    caption: 'Enter Description Here',
+    link: "https://github.com/seanbelverstone/Friend-Finder"
   },
   {
     id: 5,
     src: bamazon,
     altText: 'Bamazon',
-    caption: 'Enter Description Here'
+    caption: 'Enter Description Here',
+    link: "https://github.com/seanbelverstone/Bamazon"
   },
   {
     id: 6,
     src: liri,
     altText: 'LIRI - Node App',
-    caption: 'Enter Description Here'
+    caption: 'Enter Description Here',
+    link: "https://github.com/seanbelverstone/liri-node-app"
   },
   {
     id: 7,
     src: fridgeChef,
     altText: 'Fridge Chef',
-    caption: 'Enter Description Here'
+    caption: "Project one, what a blast. Myself and three fellow programmers created this website in just two weeks. Working in our amazing group made the whole project an absolute joy. We set off with the desire to incorporate as many of the technologies we had learnt on our first 9 weeks in the bootcamp, and we are all very satisfied with the end result. I predominantly worked on the Ingredient Entry page and had a lot of fun utilising local storage, however we all were happy to help each other when required. This page lets the user select common ingredients from their fridge and the website returns a recipe.",
+    link: "https://github.com/seanbelverstone/Fridge-Chef"
   },
   {
     id: 8,
     src: trivia,
     altText: 'Lord of the Rings Trivia',
-    caption: 'Enter Description Here'
+    caption: "A Lord of the Rings trivia quiz. With dynamically appending questions, countdown timers and a rating screen at the end, this was one of my favorite projects to work on. Not just because of the theme used, but also because this project was the first one with full creative freedom. My favorite piece of feedback from this was: 'Your UI is beautiful' So proud!",
+    link: "https://github.com/seanbelverstone/TriviaGame"
   },
   {
     id: 9,
     src: trainTimes,
     altText: 'Train Times',
-    caption: 'Enter Description Here'
+    caption: "This project contains my first ever use of the back-end service, Firebase. I am quite fond of Firebase and its intricacies and it was extremely exciting to utilize a server for the first time. I designed this page in the colors of my grandfather's favorite style of train, the Southern Railway King Arthur - Sir Lamiel.",
+    link: "https://github.com/seanbelverstone/Train-Times"
   },
   {
     id: 10,
     src: giftastic,
     altText: 'GifTastic',
-    caption: 'Enter Description Here'
+    caption: "This is a gif display website! Simply write in the name of a video game, click your shiny new button and an assortment of related gifs will display. I included an option for an amount of gifs to display for further functionality. This website required the use of APIs and AJAX calls, which were very exciting.",
+    link: "https://github.com/seanbelverstone/Giftastic"
   },
   {
     id: 11,
     src: crystal,
     altText: 'Crystal Collector',
-    caption: 'Enter Description Here'
+    caption: "A simple, yet addictive Crystal Collector game, where the user has to carefully select the crystals to reach the target number. Each crystal contains a value that is intially hidden but reveals itself on the first click. I showed this to my family back in the UK and they had a blast!",
+    link: "https://github.com/seanbelverstone/unit-4-game"
   },
   {
     id: 12,
     src: hangman,
     altText: 'UK Hangman',
-    caption: 'Enter Description Here'
+    caption: "This was my first website created during the bootcamp. I used a theme based on the wonderful United Kingdom and although I initially found certain parts of this application quite challenging, through perseverance and dedication I overcame those hurdles. I love revisiting this website because it's easy to see how far I've come since my first couple of weeks of the bootcamp!",
+    link: "https://github.com/seanbelverstone/Word-Guess-Game"
   }
 ];
 
@@ -107,6 +119,7 @@ class Projects extends Component {
     this.goToIndex = this.goToIndex.bind(this);
     this.onExiting = this.onExiting.bind(this);
     this.onExited = this.onExited.bind(this);
+    this.stopOnHover = this.stopOnHover.bind(this);
   }
 
   onExiting() {
@@ -115,6 +128,12 @@ class Projects extends Component {
 
   onExited() {
     this.animating = false;
+  }
+
+  stopOnHover() {
+    if (onmouseover(this)) {
+      this.animating=false;
+    }
   }
 
   next() {
@@ -152,7 +171,10 @@ class Projects extends Component {
             <img width='100%' src={item.src} alt={item.altText} />
           </div>
           <div className='textContainer'>
-            <CarouselCaption captionHeader={item.altText}  captionText={item.caption}  />
+            <CarouselCaption captionHeader={item.altText}  captionText={item.caption} />
+            <div className="githubLink">
+              <a href={item.link}>Click here to view the code on GitHub</a>
+            </div>
           </div>
         </div>
       </CarouselItem>
